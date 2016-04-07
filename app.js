35bdd0f243849793adb798a64f3cc18a68231184
Tomasz Marcinkowski <tmarcinkowski@blorg.pl>
2016-04-07 10:17:55 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+angular.module('addressbook', ['ui.bootstrap']);
+angular.module('addressbook').controller('MainCtrl', function ($scope) {
+
+  $scope.model = {
+    data: 'Please paste your data here'
+  };
+});
